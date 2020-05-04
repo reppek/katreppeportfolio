959e54106e134a420ece3673871ecaa459da9ab6
reppek <54945763+reppek@users.noreply.github.com>
2020-05-04 14:26:39 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+function changePic1(my_image)
+    {
+
+        my_image.src = 'someimage2.jpg';
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changePic1()
+{
+ $("#my-img").attr('src', '../images/mural-pic.png');
+}
+
+function offHover()
+{
+  $("#my-img").attr('src', '../images/selfportrait2.png');
+}
+
+function changePic1(my_image)
+{
+	my_image.src = "../images/mural-pic.png";
+}
+
+
+function changePic1() {
+document.getElementById("myImg").src = "../images/mural-pic.png";
+   }
+  function changePic2(){
+   document.getElementById("myImg").src = "../images/selfportrait2.png";
+   }
